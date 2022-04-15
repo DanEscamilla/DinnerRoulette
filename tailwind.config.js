6273be65d734d273fc908e778928ca88b5c7bfeb
daniel <danielcarbajalescamilla@gmail.com>
2022-04-14 20:24:53 -0600

    Add primary shades

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
       colors: {
         warning: '#b15000',
         gold: '#f3aa14',
-        primary: '#178752',
+        primary: {
+          DEFAULT: '#1B9D5F',
+          50: '#B5F2D5',
+          100: '#A4EFCB',
+          200: '#81E9B8',
+          300: '#5EE4A4',
+          400: '#3BDE90',
+          500: '#23CD7C',
+          600: '#1B9D5F',
+          700: '#136D42',
+          800: '#0A3D25',
+          900: '#020D08',
+        },
       },
       animation: {
         draw: 'draw 5s ease-out',
