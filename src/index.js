c543be4f4cedc87f6dfb861138093b252686d3de
daniel <danielcarbajalescamilla@gmail.com>
2022-04-23 14:17:02 -0600

    Add theme provider

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.css';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 import ActionButton from './content_script_app/ActionButton';
+import './index.css';
 
 const dinnerRouletteRootElement = document.createElement('div');
 dinnerRouletteRootElement.setAttribute('id', 'dinner-roulette-root');
 
 document.body.appendChild(dinnerRouletteRootElement);
 
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    type: 'dark',
+    primary: {
+      main: '#1b9d5f',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <ActionButton />
+    <ThemeProvider theme={theme}>
+      <ActionButton />
+    </ThemeProvider>
   </React.StrictMode>,
   dinnerRouletteRootElement
 );
